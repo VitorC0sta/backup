0a91a2701fb70d840778dac1827884e6d5a84e9e
Vitor Costa <vitor.costafelippe@gmail.com>
2023-08-30 13:38:55 -0300

    08/30/23 - Promisses

--- 3AsyncJS/promises/main.js
+++ 3AsyncJS/promises/main.js
@@ -1,30 +1,30 @@
-// const randomTime = (min, max) => Math.floor(Math.random() * (max-min) + min);
 
+const randomTime = (min, max) => Math.floor(Math.random() * ((max*1000)-(min*1000)) + (min*1000));
 
-// function responseTimeSimulation(message) {
-//     const time = randomTime(1000,3000);
+ function responseTimeSimulation(message) {
+    const time = randomTime(1,3);
 
-//     const response = `Time: ${(time/1000).toFixed(2)}s | ${message}`;
+    const response = `Time: ${(time/1000).toFixed(2)}s | ${message}`;
 
-//     return new Promise((resolve, rejected) => {
-//         setTimeout(() => {
-//             resolve(response);
-//         }, time); 
-//     });
+    return new Promise((resolve, rejected) => {
+        setTimeout(() => {
+            resolve(response);
+        }, time); 
+    });
     
-// }
+}
 
-// responseTimeSimulation("Conected 1").then(response => {
-//     console.log(response);        
-//     return responseTimeSimulation("Conected 2"); 
-// }).then(response => {
-//     console.log(response);
-//     return responseTimeSimulation("Conected 3");
-// }).then(response=> {
-//     console.log(response);
-//     return responseTimeSimulation("Conected 4");
-// }).catch();
+responseTimeSimulation("Conected 1").then(response => {
+    console.log(response);        
+    return responseTimeSimulation("Conected 2"); 
+}).then(response => {
+    console.log(response);
+    return responseTimeSimulation("Conected 3");
+}).then(response=> {
+    console.log(response);
+    return responseTimeSimulation("Conected 4Ã§Ã§");
+}).then(response => {
+    console.log(response);
+}).catch();
+ 
 
-const text = "Exemplo com acentuação: usuário cachaça áéíóú";
-
-console.log(text);
